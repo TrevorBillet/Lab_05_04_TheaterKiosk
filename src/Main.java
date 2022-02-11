79ccf4969ffaa26e2864784cbf47e257732769d3
Trevor Billet <98602126+TrevorBillet@users.noreply.github.com>
2022-02-11 16:22:37 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String ofAge = "Y";
+        ofAge = "N";
+        if (ofAge == "Y")
+            System.out.println("You get a wristband!");
+        else
+            System.out.println("Enjoy the movie!");
+    }
+}
